6d306efcf5646412b4256122142c802fa0bcca83
unknown <edfournier@umass.edu>
2024-07-30 18:18:14 -0400

    fix error handler

--- common/lib/error.js
+++ common/lib/error.js
@@ -1,11 +1,11 @@
 import logger from "./logger.js";
 
 export function errorHandler(err, req, res, next) {
-    logger.error(`Process ${process.pid} encountered an error`, err);
+    logger.error(`Process ${process.pid} encountered an error:`, err);
     if (req.headersSent) {
         next(err);
     }
     else {
-        res.status(err.status || 500).json({ error: "Internal Server Error" });
+        res.status(500).json({ error: "Internal Server Error" });
     }
 }   
