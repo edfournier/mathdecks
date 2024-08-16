aeb2d1c2b1305f8a3072a03cbc5d3deedafe7993
unknown <edfournier@umass.edu>
2024-08-16 01:30:31 -0400

    comment

--- client/src/lib/logout.js
+++ client/src/lib/logout.js
@@ -3,6 +3,9 @@ import deckStore from "$lib/stores/deck-store.js";
 import userStore from "$lib/stores/user-store.js";
 import viewStore from "$lib/stores/view-store.js";
 
+/**
+ * Preforms all clean-up tasks needed on log out and redirects to login
+ */
 export function logout() {
     // Reset all stores
     deckStore.reset();
