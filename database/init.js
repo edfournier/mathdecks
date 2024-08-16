a054da3970ad2d4a3c809b4c35bd6a7634c83644
unknown <edfournier@umass.edu>
2024-08-16 12:39:23 -0400

    env files

--- database/init.js
+++
@@ -1,5 +0,0 @@
-// Create initial databases and collections
-const mongo = db.getMongo();
-mongo.getDB("auth-db").createCollection("users");
-mongo.getDB("deck-db").createCollection("decks");
-mongo.getDB("milestone-db").createCollection("records");
